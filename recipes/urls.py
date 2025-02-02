05a03899e46327b45df7d3ec5e246574c4930749
mohamedmostafa33 <mohamedabohamama15@gmail.com>
2025-02-03 01:20:34 +0200

    add and finish DetailView and CreateView classed to view and add new recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.landing, name="recipes-landing"),
-    path('home/', views.home, name="recipes-home"),
+    path('home/', views.RecipeListView.as_view(), name="recipes-home"),
+    path('recipe/<int:pk>', views.RecipeDetailView.as_view(), name="recipe-detail"),
+        path('recipe/create/', views.RecipeCreateView.as_view(), name="recipe-create"),
     path('about/', views.about, name="recipes-about"),
+
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,19 +1,48 @@
 from django.shortcuts import render
 from . import models
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+
 
 
 def landing(request):
     return render(request, 'recipes/index.html', {'title' : 'Recipes'})
 
+### -- Function based home view -- ###
+# @login_required
+# def home(request):
+#     recipes = models.Recipe.objects.all()
+#     context = {
+#         'recipes' : recipes
+#     }
+#     return render(request ,'recipes/home.html', context)
 
-@login_required
-def home(request):
-    recipes = models.Recipe.objects.all()
-    context = {
-        'recipes' : recipes
-    }
-    return render(request ,'recipes/home.html', context)
+
+### -- Class based home view -- ###
+class RecipeListView(LoginRequiredMixin, ListView):
+    model = models.Recipe
+    template_name = 'recipes/home.html'
+    context_object_name = 'recipes'
+
+
+class RecipeDetailView(LoginRequiredMixin, DetailView):
+    model = models.Recipe
+    template_name = 'recipes/recipe_detail.html'
+    context_object_name = 'recipe'
+    
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+    model = models.Recipe
+    template_name = 'recipes/recipe_create.html'
+    fields = ['title', 'description', 'image']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse_lazy('recipe-detail', kwargs={'pk':self.object.pk})
 
 def about(request):
     return render(request, 'recipes/about.html', {'title' : 'About us'})
