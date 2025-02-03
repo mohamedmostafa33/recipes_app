3315db33a25d8612e8691dfc50cabaa4a5a5967b
mohamedmostafa33 <mohamedabohamama15@gmail.com>
2025-02-03 15:24:06 +0200

    Finish DeleteView for recipes delete

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('about/', views.about, name="recipes-about"),
     path('recipe/<int:pk>', views.RecipeDetailView.as_view(), name="recipe-detail"),
     path('recipe/create/', views.RecipeCreateView.as_view(), name="recipe-create"),
-    path('recipe/<int:pk>/update/', views.RecipeUpdateView.as_view(), name="recipe-update")
+    path('recipe/<int:pk>/update/', views.RecipeUpdateView.as_view(), name="recipe-update"),
+    path('recipe/<int:pk>/delete/', views.RecipeDeleteView.as_view(), name="recipe-delete")
 ]
